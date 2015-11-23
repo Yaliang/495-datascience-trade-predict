1a6fc2fd58d655ed3893561556105a40358b1374
Harry Ji <xiangyu.ji@outlook.com>
2015-11-23 17:39:10 -0600

    linear regression model

--- prediction/predict.py
+++ prediction/predict.py
@@ -29,7 +29,7 @@ def get_input(URL):
     label = list()
 
     # print "get_data:",get_data
-    for k in get_data[1:10]:
+    for k in get_data[1:11]:
         temp = k[1:26]
         temp_label = k[26]
         feature.append(temp)
@@ -38,7 +38,7 @@ def get_input(URL):
 
     test_f = list()
     test_label = list()
-    for k in get_data[10:]:
+    for k in get_data[11:]:
         test_f.append(k[1:26])
         test_label.append(k[26])
 
@@ -51,17 +51,43 @@ def get_input(URL):
 
 
 
+def get_square_error(data,prediction):
+    add = 0
+    for index in range(len(data)):
+        add += (abs(data[index]-prediction[index]))/data[index]
+    return add/len(data)
+
+
 if __name__ == '__main__':
     feature, label, test_f, test_label = get_input('totalTable.csv')
-
-    print feature
     print feature.shape
 
-    select_top_k_feature = SelectKBest(chi2,5)
+    print "begin"
+    all_index = list()
+    all_value = list()
+    for k in range(1,25):
+        all_index.append(k)
+        select_top_k_feature = SelectKBest(chi2,k)
+        linear = LinearRegression()
+        _top = list()
 
-    _top = list()
-    for data in (select_top_k_feature.fit(feature,label).scores_):
-        _top.append(data)
-    print _top
-    print heapq.nlargest(5,zip(_top,itertools.count()))
-    print heapq.nlargest(5,range(len(_top)),key = _top.__getitem__)
+        for data in (select_top_k_feature.fit(feature,label).scores_):
+            _top.append(data)
+        index = heapq.nlargest(k,range(len(_top)),key = _top.__getitem__)
+        test_feature = list()
+
+        print "index_list:",index
+        for id_index in range(len(list(test_f))):
+            temp = list()
+            for id in index:
+                temp.append(test_f[id_index][id])
+            test_feature.append(temp)
+        test_feature = asarray(test_feature)
+
+        predict_value = linear.fit(select_top_k_feature.fit(feature,label).fit_transform(feature,label),label).predict(test_feature)
+        value = get_square_error(list(test_label),list(predict_value))
+        print value
+        all_value.append(value)
+    min_error = min(all_value)
+    result_num = all_value.index(min_error)
+    print all_index[result_num]
